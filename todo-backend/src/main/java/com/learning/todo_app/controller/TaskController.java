2576bfa07056fba87e09d2eddc6094d488fdbcf2
sulthonmufti <msulthonmufti14@gmail.com>
2026-04-22 21:53:00 +0700

    refactor: update TaskController formatting and improve todo-frontend UI

--- todo-backend/src/main/java/com/learning/todo_app/controller/TaskController.java
+++ todo-backend/src/main/java/com/learning/todo_app/controller/TaskController.java
@@ -10,11 +10,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-//@CrossOrigin(origins = "*") //agar semua domain bisa akses backend ini
+// @CrossOrigin(origins = "*") //agar semua domain bisa akses backend ini
 @CrossOrigin(origins = {
-    "http://localhost:3000", 
-    "http://10.66.90.225:3000" //domain lain yang akan diizinkan (di ZeroTier pribadi)
-}) //agar react nanti bisa akses backend ini
+        "http://localhost:3000",
+        "http://10.66.90.225:3000" // domain lain yang akan diizinkan (di ZeroTier pribadi)
+}) // agar react nanti bisa akses backend ini
 public class TaskController {
 
     @Autowired
